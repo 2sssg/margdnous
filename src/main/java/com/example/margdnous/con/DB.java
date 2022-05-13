69eb53935cfcc6f2dbd4009c444068affa84d791
2sssg <lsg020300@naver.com>
2022-05-13 16:13:20 +0900

    Feat: DB Setting

---
+++ src/main/java/com/example/margdnous/con/DB.java
@@ -0,0 +1,48 @@
+package com.example.margdnous.con;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@Slf4j
+public class DB {
+    private Statement stmt;
+    private DB(){
+        try{
+            Connection con = DriverManager.getConnection(INFORMATION.DBURL,INFORMATION.DBID,INFORMATION.DBPW);
+            this.stmt = con.createStatement();
+        }catch(Exception e){
+            log.error("     DB CONNECTION ERROR");
+            e.printStackTrace();
+        }
+    }
+    private static class InnerClass{
+        private static final DB db = new DB();
+    }
+    public static DB getInstance(){return InnerClass.db;}
+
+    // select query 날릴때
+    public ResultSet selectQuery(String query){
+        ResultSet resultSet=null;
+        try{
+            resultSet = stmt.executeQuery(query);
+        }catch (Exception e){
+            log.error("        query Error");
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    // create, update 같은거 날릴 때
+    public void updateQuery(String query){
+        try{
+            stmt.executeUpdate(query);
+        }catch (Exception e){
+            log.error("        query Error");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/example/margdnous/con/INFORMATION.java
@@ -0,0 +1,7 @@
+package com.example.margdnous.con;
+
+public class INFORMATION {
+    static String DBURL = "jdbc:mysql://101.101.219.145:3306/rs_project?serverTimezone=Asia/Seoul";
+    static String DBID = "rs_team";
+    static String DBPW = "sodlfmadmsfbehddls";
+}
