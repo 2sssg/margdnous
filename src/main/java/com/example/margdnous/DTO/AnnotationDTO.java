3b91e412970aeb5c6431b734398088a9cfb09ee2
2sssg <lsg020300@naver.com>
2022-05-13 23:21:29 +0900

    Feat:DTO

--- src/main/java/com/example/margdnous/DTO/AnnotationDTO.java
+++ src/main/java/com/example/margdnous/DTO/AnnotationDTO.java
@@ -1,18 +1,23 @@
 package com.example.margdnous.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.example.margdnous.model.ModelInterface;
+import lombok.*;
 
 import javax.persistence.Column;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class AnnotationDTO {
-    private long id;
+@Getter
+@Setter
+public class AnnotationDTO extends DTO{
     private int categoryID;
     private String category;
     private int labelID;
     private String labelNAME;
+
+    public AnnotationDTO(long id, int categoryID, String category, int labelID, String labelNAME) {
+        super(id);
+        this.categoryID = categoryID;
+        this.category = category;
+        this.labelID = labelID;
+        this.labelNAME = labelNAME;
+    }
 }
--- src/main/java/com/example/margdnous/DTO/DTO.java
+++ src/main/java/com/example/margdnous/DTO/DTO.java
@@ -1,2 +1,10 @@
-package com.example.margdnous.DTO;public class DTO {
+package com.example.margdnous.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+public class DTO {
+    private long id;
+
 }
