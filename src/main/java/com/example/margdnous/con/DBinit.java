ac4047d3e2a0c7d19046b2e851878990647178ff
2sssg <lsg020300@naver.com>
2022-05-14 09:57:13 +0900

    temp

--- src/main/java/com/example/margdnous/con/DBinit.java
+++ src/main/java/com/example/margdnous/con/DBinit.java
@@ -43,7 +43,7 @@ public class DBinit {
     }
     public void makeSqlFile() throws IOException, ParseException {
         makeSQL();
-        String filePath = "/Users/2sssg/workspace/margdnous/src/main/resources/import.sql";
+        String filePath = "/src/main/resources/import.sql";
         File file = new File(filePath);
         file.delete();
         file = new File(filePath);
